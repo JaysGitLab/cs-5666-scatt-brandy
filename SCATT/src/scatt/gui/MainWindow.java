58a7ae578f46fe711cbbceb1e8f04734aff40347
Matt Stone <mattstone22133@gmail.com>
2017-04-19 07:56:58 -0400

    Made a dynamic configuration panel. This can generate buttons, so we can generate buttons to configure each of our modules based on the modues we set for the grader component.

--- SCATT/src/scatt/gui/MainWindow.java
+++ SCATT/src/scatt/gui/MainWindow.java
@@ -8,7 +8,6 @@ import java.awt.GridBagLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
-import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 /**
@@ -26,7 +25,6 @@ public class MainWindow extends JFrame
 	 */
     private static final long serialVersionUID = -39970827731145361L;
     private JPanel contentPane;
-    private JTextField textField;
 
     // private JFileChooser dirChooser;
     // private JFileChooser fileChooser;
@@ -63,91 +61,14 @@ public class MainWindow extends JFrame
         JPanel loader = new LoaderPanel();
         load.add(loader);
 
-        JPanel configure = new JPanel();
+        // JPanel configure = new JPanel();
+        JPanel configure = new ConfigurePanel();
         tabbedPane.addTab("Configure", null, configure, null);
-        configure.setLayout(null);
-
-        textField = new JTextField();
-        textField.setBounds(144, 77, 114, 19);
-        configure.add(textField);
-        textField.setColumns(10);
 
         // JPanel grade = new JPanel();
         JPanel grade = new GradePanel();
         tabbedPane.addTab("Grade", null, grade, null);
-
-        // dirChooser = new JFileChooser();
-        // dirChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-        //
-        // fileChooser = new JFileChooser();
-        // fileChooser
-        // .setFileFilter(new FileNameExtensionFilter("sb2 file", "sb2"));
-        //
-        // JButton btnJfilechoser = new JButton(".sb2 directory");
-        //
-        // // The function called when the button is pressed.
-        // btnJfilechoser.addActionListener(new ActionListener()
-        // {
-        // public void actionPerformed(ActionEvent arg0)
-        // {
-        // openDirectoryFromFileMenu();
-        // }
-        // });
-        // Load.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-        //
-        // Load.add(btnJfilechoser);
-        //
-        // JButton btnSinglesb = new JButton("single .sb2");
-        // btnSinglesb.addActionListener(new ActionListener()
-        // {
-        // public void actionPerformed(ActionEvent arg0)
-        // {
-        // openFileFromFileMenu();
-        // }
-        // });
-        //
-        // Load.add(btnSinglesb);
-
-        //
-        // }
-        // protected void openDirectoryFromFileMenu()
-        // {
-        // // open file chooser and then check if they approved a file
-        // if (dirChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
-        // {
-        // File dir = dirChooser.getSelectedFile();
-        // System.out.println("opened: " + dir.getAbsolutePath());
-        // System.out.println("printing found .sb2 files in directory");
-        //
-        // // find every .sb2 in this directory and load them into memory
-        // // TODO (INCOMPLETE)
-        // for (File file : dir.listFiles())
-        // {
-        // String path = file.getAbsolutePath();
-        // if (path.substring(path.length() - 4).equals(".sb2"))
-        // {
-        // // create a student object? printing for now
-        // System.out.println(file.getAbsolutePath());
-        // }
-        // }
-        // }
-        // }
-        //
-        // /**
-        // * Test method that opens up a single file from the file menu.
-        // */
-        // protected void openFileFromFileMenu()
-        // {
-        // // open file chooser and then check if they approved a file
-        // if (fileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
-        // {
-        // File openedSb2 = fileChooser.getSelectedFile();
-        // // below is an example of how it might look to create a student
-        // // Student student = new Student(openedSb2.getAbsolutePath());
-        // System.out.println(openedSb2.getAbsolutePath());
-        //
-        // }
-
+        
     }
 
     /**
